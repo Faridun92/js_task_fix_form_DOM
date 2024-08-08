abc9e0f3e6442a2a2835dc14e0920dca7125ba16
Mainaiev <farid.mainaiev@proton.me>
2024-08-08 20:35:12 +0300

    make build

---
+++ index.90d92c89.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll("input");document.addEventListener("DOMContentLoaded",function(){e.forEach(function(e){var t=document.createElement("label");t.classList.add("field-label"),t.setAttribute("for",e.id),t.textContent=e.name,e.placeholder=e.name.charAt(0).toUpperCase()+e.name.slice(1),e.parentNode.insertBefore(t,e)})});
+//# sourceMappingURL=index.90d92c89.js.map
